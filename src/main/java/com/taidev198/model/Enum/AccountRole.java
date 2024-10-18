27292c9717100d31035eee500a5bf297e408db04
traigiuxom9x@gmail.com <traigiuxom9x@gmail.com>
2024-10-18 14:19:51 +0700

    adding viewer role

--- src/main/java/com/taidev198/model/Enum/AccountRole.java
+++ src/main/java/com/taidev198/model/Enum/AccountRole.java
@@ -3,13 +3,15 @@ package com.taidev198.model.Enum;
 public enum AccountRole {
     ADMIN,
     CUSTOMER,
-    SELLER;
+    SELLER,
+    VIEWER;
 
     public int getIndex() {
         return switch (this) {
             case ADMIN -> 0;
             case CUSTOMER -> 1;
             case SELLER -> 2;
+            case VIEWER -> 3;
         };
     }
 }
